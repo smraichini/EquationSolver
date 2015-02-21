2a081654a8c296f6f14560d7d8850e334e002d09
smraichini <siamon100@gmail.com>
2015-02-21 11:48:17 +0100

    insertCoefficent

--- main.c
+++ main.c
@@ -7,12 +7,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-/*
- * 
- */
+struct num{
+    float real;
+    float immaginary;
+};
+
+typedef struct num number;
+void insertCoefficent(int *a,int *b,int *c);
+
+void insertCoefficent(int *a,int *b,int *c)
+{
+printf("Inserici il il coefficente A: ");
+    scanf("%d",a);
+    
+    printf("Inserici il il coefficente B: ");
+    scanf("%d",b);
+    
+    printf("Inserici il il coefficente C: ");
+    scanf("%d",c);
+}
+
 int main(int argc, char** argv) {
+    int a,b,c;
+    insertCoefficent(&a,&b,&c);
+    solutionCalculation(a,b,c);
+    printSolution
 
+    
     return (EXIT_SUCCESS);
 }
 
