8f200e66d3d8a8d3cf4df9bd9b3f00dc7975efc0
smraichini <siamon100@gmail.com>
2015-02-21 11:26:23 +0100

    start projct

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Simone
+ *
+ * Created on 21 febbraio 2015, 11.24
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
